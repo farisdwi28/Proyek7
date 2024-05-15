bb024b7cc27c23cb4df0157f3ae44501048fffd7
DimasRP1 <dimas.rizky.prayoga@gmail.com>
2024-05-16 03:30:21 +0700

    Fix Primary Colors

--- alfariz_property/lib/utils/constants/colors.dart
+++ alfariz_property/lib/utils/constants/colors.dart
@@ -4,7 +4,7 @@ class TColors {
   TColors._();
 
   // App Basic Colors
-  static const Color primary = Color(0xFF4B68FF);
+  static const Color primary = Color(0xff153448);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
@@ -20,9 +20,9 @@ class TColors {
   );
 
   // Text Colors
-  static const Color textPrimary = Color(0xFF4B68FF);
-  static const Color textSecondary = Color(0xFF4B68FF);
-  static const Color textWhite = Color(0xFF4B68FF);
+  static const Color textPrimary = Color(0xff153448);
+  static const Color textSecondary = Color(0xff153448);
+  static const Color textWhite = Color(0xff153448);
 
   // Backround Colors
   static const Color light = Color(0xFFF6F6F6);
@@ -34,13 +34,13 @@ class TColors {
   static Color darkContainer = TColors.accent.withOpacity(0.1);
 
   // Button Colors 
-  static const Color buttonPrimary = Color(0xFF4B68FF);
-  static const Color buttonSecondary = Color(0xFF4B68FF);
-  static const Color buttonDisabled = Color(0xFF4B68FF);
+  static const Color buttonPrimary = Color(0xff153448);
+  static const Color buttonSecondary = Color(0xff153448);
+  static const Color buttonDisabled = Color(0xff153448);
 
   // Border Colors
-  static const Color borderPrimary = Color(0xFF4B68FF);
-  static const Color borderSecondary = Color(0xFF4B68FF);
+  static const Color borderPrimary = Color(0xff153448);
+  static const Color borderSecondary = Color(0xff153448);
 
   // Error and Validation Colors 
   static const Color error = Color(0xFFD32F2F);
